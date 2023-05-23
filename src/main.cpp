7237ed82a15114d4cd215e33fbd62918a8d28258
александр кунцевич <akountsevich@gmail.com>
2023-05-23 16:51:55 +0300

    added my menu implementations (no idea if it works but my menu stuff is not integrated in game loop)

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,12 @@
 #include "game.h"
 #include "car.h"
 extern "C"
+#define SDL_MAIN_HANDLED
 //--------------------------------------------------------------------------
 int main(int argc, char* argv[]) 
 {
     Game Races;
+    SDL_Init(SDL_INIT_EVERYTHING);
     SDL_SetMainReady();
 
     return Races.Execute();
--- src/menu.cpp
+++ src/menu.cpp
@@ -1,5 +1,5 @@
 #include "Menu.h"
-//#include <SDL_ttf.h>
+#include <SDL_ttf.h>
 Menu::Menu(SDL_Renderer* renderer)
     : m_renderer(renderer), m_currentMenuOption(MenuOption::START_GAME), m_exit(false) {}
 
@@ -26,10 +26,10 @@ void Menu::handleInput(SDL_Event event) {
                 // Select a menu option
                 switch (m_currentMenuOption) {
                     case MenuOption::START_GAME:
-                        // (initialise the game process)
+                        // (initialise the game process in game loop)
                         break;
                     case MenuOption::LEADERBOARD:
-                        // (show the leaderboard)
+                        // (load the leaderboard)
                         break;
                     case MenuOption::EXIT_GAME:
                         m_exit = true;
@@ -40,11 +40,27 @@ void Menu::handleInput(SDL_Event event) {
     }
 }
 
+void Menu::renderMenuOption(const char* text, int x, int y, bool isSelected) {
+    SDL_Color color;
+        if (isSelected) {
+            // If it is selected, make it white
+            color = {255, 255, 255, 255};
+        } else {
+            // If it is not selected, make it grey
+            color = {128, 128, 128, 255};
+        }
+}
+//menu::render should be in game loop cuz it changes on each key press
 void Menu::render() {
     SDL_SetRenderDrawColor(m_renderer, 0, 0, 0, 0);
-    SDL_RenderClear(m_renderer);
-}
+    SDL_RenderClear(m_renderer); //clearing renderer before making the menu options 
 
-/*bool Menu::shouldExit() const {
-    return m_exit
-}*/
+    renderMenuOption("Start Game", 100, 100, m_currentMenuOption == MenuOption::START_GAME);
+    renderMenuOption("Leaderboard", 100, 200, m_currentMenuOption == MenuOption::LEADERBOARD);
+    renderMenuOption("Exit Game", 100, 300, m_currentMenuOption == MenuOption::EXIT_GAME);
+    // render all 3 buttons (selected is white, rest is grey)
+}
+// getter for m_exit, should have a function in game loop that closes the game as soon as m_exit is true
+bool Menu::shouldExit() const {
+    return m_exit;
+}
